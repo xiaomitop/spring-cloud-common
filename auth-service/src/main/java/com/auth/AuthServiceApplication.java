a0260aeb64dbddb0ba363d90b1b9e1abdb864a03
yangt <yangtaotop@126.com>
2018-04-24 18:31:44 +0800

    加如注册中心，网关，配置中心等

--- auth-service/src/main/java/com/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/auth/AuthServiceApplication.java
@@ -2,10 +2,12 @@ package com.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthServiceApplication {
 
